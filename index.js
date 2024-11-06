73676911cd3249566434b169bd10fb1fb6b0a8f6
GustavoFSProg <gustavo.prog40@gmail.com>
2024-11-06 19:52:39 -0300

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import userController from './userController'
+import userController from './src/userController'
 
 dotenv.config()
 
